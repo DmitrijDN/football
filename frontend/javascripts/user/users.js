49a3a0f8f2101ff1602314ecb5978e2df7464f6c
Dmitriy Beseda <besedadg@gmail.com>
2016-04-07 15:15:24 +0300

    Added default user array

---
+++ frontend/javascripts/user/users.js
@@ -0,0 +1,17 @@
+var players = [
+	'Andrey Karun',
+	'Anton Duzenko',
+	'Artyom Goncharov',
+	'Bogdan Rusinka',
+	'Dima Beseda',
+	'Dima Yezhelev',
+	'Edik Dolynskiy',
+	'Lyubomir Balabanskiy',
+	'Max Goncharuk',
+	'Misha Morozov',
+	'Nikita Semenistiy',
+	'Pavel Filatov',
+	'Sanya Kovalov',
+	'Ihor Habshiy',
+	'Alexei Vdovichenko'
+];
